98dcad096ccfb2d29ce8078a7dcb46fb3958b2d9
Alex Gaynor <alex.gaynor@gmail.com>
2017-04-22 21:34:02 -0400

    simplify

--- src/main.rs
+++ src/main.rs
@@ -160,12 +160,10 @@ fn submit_cert_to_logs<'a>(http_client: &hyper::Client,
             .unwrap();
 
     return logs.par_iter()
-               .map(|ref log| {
-                        let sct = submit_to_log(http_client, &log.url, &payload);
-                        return (log.clone(), sct);
-                    })
-               .filter(|&(_, ref sct)| sct.is_some())
-               .map(|(log, sct)| (log, sct.unwrap()))
+               .filter_map(|ref log| {
+                               let sct = submit_to_log(http_client, &log.url, &payload);
+                               return sct.map(|s| (log.clone(), s));
+                           })
                .collect();
 }
 
