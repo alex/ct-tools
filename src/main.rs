31c3f911b1d18019ce0d10a0c17cad2a172dfe5a
Alex Gaynor <alex.gaynor@gmail.com>
2017-05-05 22:11:21 -0400

    Submit multiple certs/chains at once

--- src/main.rs
+++ src/main.rs
@@ -28,66 +28,71 @@ fn pems_to_chain(data: &str) -> Vec<Vec<u8>> {
                .collect::<Vec<_>>();
 }
 
-fn main() {
-    let path = match env::args().skip(1).next() {
-        Some(p) => p,
-        None => {
-            println!("Usage: {} <path/to/cert-or-chain.pem>", env::args().next().unwrap());
-            process::exit(1);
-        }
-    };
-
-    let mut contents = String::new();
-    File::open(path)
-        .unwrap()
-        .read_to_string(&mut contents)
+fn build_chain(http_client: &hyper::Client, cert: &[u8]) -> Vec<Vec<u8>> {
+    let body = url::form_urlencoded::Serializer::new(String::new())
+        .append_pair("b64cert", &base64::encode(cert))
+        .finish();
+    let body_bytes = body.as_bytes();
+    let response = http_client
+        .post("https://crt.sh/gen-add-chain")
+        .header(hyper::header::ContentType::form_url_encoded())
+        .body(hyper::client::Body::BufBody(body_bytes, body_bytes.len()))
+        .send()
         .unwrap();
 
-    let mut chain = pems_to_chain(&contents);
+    let add_chain_request: AddChainRequest = serde_json::from_reader(response).unwrap();
+    return add_chain_request
+        .chain
+        .iter()
+        .map(|c| base64::decode(c).unwrap())
+        .collect();
+}
+
+fn main() {
+    if env::args().len() == 1 {
+        println!("Usage: {} <cert-or-chain.pem ...>", env::args().next().unwrap());
+        process::exit(1);
+    }
 
     let mut http_client = hyper::Client::with_connector(
         hyper::net::HttpsConnector::new(hyper_native_tls::NativeTlsClient::new().unwrap())
     );
+    let logs = fetch_trusted_ct_logs(&http_client);
 
-    if chain.len() == 1 {
-        // TODO: There's got to be some way to do this ourselves, instead of using crt.sh as a
-        // glorified AIA chaser.
-        println!("Only one certificate in chain, using crt.sh to build a full chain");
-        let body = url::form_urlencoded::Serializer::new(String::new())
-            .append_pair("b64cert", &base64::encode(&chain[0]))
-            .finish();
-        let body_bytes = body.as_bytes();
-        let response = http_client
-            .post("https://crt.sh/gen-add-chain")
-            .header(hyper::header::ContentType::form_url_encoded())
-            .body(hyper::client::Body::BufBody(body_bytes, body_bytes.len()))
-            .send()
+    for path in env::args().skip(1) {
+        println!("Submitting {} ...", path);
+
+        let mut contents = String::new();
+        File::open(path)
+            .unwrap()
+            .read_to_string(&mut contents)
             .unwrap();
 
-        let add_chain_request: AddChainRequest = serde_json::from_reader(response).unwrap();
-        chain = add_chain_request
-            .chain
-            .iter()
-            .map(|c| base64::decode(c).unwrap())
-            .collect();
+        let mut chain = pems_to_chain(&contents);
+        if chain.len() == 1 {
+            // TODO: There's got to be some way to do this ourselves, instead of using crt.sh as a
+            // glorified AIA chaser.
+            println!("Only one certificate in chain, using crt.sh to build a full chain ...");
+            chain = build_chain(&http_client, &chain[0]);
+        }
+        let scts = submit_cert_to_logs(&http_client, &logs, &chain);
+
+        let mut table = prettytable::Table::new();
+        table.add_row(row!["Log", "SCT"]);
+        for (log, sct) in scts {
+            table.add_row(row![log.description, base64::encode(&sct.to_raw_bytes())]);
+        }
+        table.printstd();
+
+        // TODO: is there a better way to do this hex-encoding?
+        println!("Find the cert on crt.sh: https://crt.sh?q={}",
+                 digest::digest(&digest::SHA256, &chain[0])
+                     .as_ref()
+                     .iter()
+                     .map(|b| format!("{:02X}", b))
+                     .collect::<String>());
+         println!();
+         println!();
     }
 
-    http_client.set_read_timeout(Some(Duration::from_secs(5)));
-    let logs = fetch_trusted_ct_logs(&http_client);
-    let scts = submit_cert_to_logs(&http_client, &logs, &chain);
-
-    let mut table = prettytable::Table::new();
-    table.add_row(row!["Log", "SCT"]);
-    for (log, sct) in scts {
-        table.add_row(row![log.description, base64::encode(&sct.to_raw_bytes())]);
-    }
-    table.printstd();
-
-    // TODO: is there a better way to do this hex-encoding?
-    println!("Find the cert on crt.sh: https://crt.sh?q={}",
-             digest::digest(&digest::SHA256, &chain[0])
-                 .as_ref()
-                 .iter()
-                 .map(|b| format!("{:02X}", b))
-                 .collect::<String>());
 }
