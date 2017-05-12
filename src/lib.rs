d1590c939408b2dd00087752daaed0b17e18e2de
Alex Gaynor <alex.gaynor@gmail.com>
2017-05-12 19:50:03 -0400

    First pass at automatic cert issuance with LE. Untested.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+extern crate acme_client;
 extern crate base64;
 extern crate byteorder;
+extern crate chrono;
 extern crate hex;
 extern crate hyper;
+extern crate openssl;
 extern crate rayon;
 extern crate ring;
+extern crate rustls;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
@@ -14,3 +18,4 @@ pub mod common;
 pub mod crtsh;
 pub mod ct;
 pub mod google;
+pub mod letsencrypt;
