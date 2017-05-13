2ebf9bb3bfa86f980a305bc977e0c6bb0157f1fb
Alex Gaynor <alex.gaynor@gmail.com>
2017-05-13 17:27:19 -0400

    Start hacking on an HTTPS server that submits client certs (#3)
    * Start hacking on an HTTPS server that submits client certs
    
    * More hacking
    
    * cargo fmt
    
    * Boom, this works now!
    
    * Render the cert if we have one (in theory)
    
    * Submit to CT logs on POST!
    
    * words
    
    * link to crt.sh on successful submission
    
    * Log a message from the server
    
    * Style improvements
    
    * Use my fork of rustls, it works!
    
    * A comment
    
    * appease the linter
    
    * more linter appeasement
    
    * more linter appeasement
    
    * cargo update
    
    * Automatic cert issuance with LE (#5)
    
    * Start hacking on an HTTPS server that submits client certs
    
    * More hacking
    
    * Use my fork of rustls, it works!
    
    * Get rid of cell.
    
    * First pass at automatic cert issuance with LE. Untested.
    
    * Added actual content
    
    * Update comment, fix warning
    
    * Fix that tomprince pointed out
    
    * Wire up configuration
    
    * mut is unnecseary
    
    * sigh, 443
    
    * Set a subject and an issuer on the cert
    
    * Set validity on our dummy cert
    
    * todo
    
    * FUCK. inverted logic
    
    * Use lots of threads
    
    * rustfmt
    
    * where does this come from...
    
    * clippy
    
    * cargo fmt
    
    * Parameterize letsencrypt env
    
    * TODO completed
    
    * clippy
    
    * clippy
    
    * cargo update
    
    * More hacking
    
    * Use my fork of rustls, it works!
    
    * First pass at automatic cert issuance with LE. Untested.
    
    * Added actual content
    
    * Get rid of cell.
    
    * Update comment, fix warning
    
    * Fix that tomprince pointed out
    
    * Wire up configuration
    
    * mut is unnecseary
    
    * sigh, 443
    
    * Set a subject and an issuer on the cert
    
    * Set validity on our dummy cert
    
    * todo
    
    * FUCK. inverted logic
    
    * Use lots of threads
    
    * rustfmt
    
    * where does this come from...
    
    * clippy
    
    * cargo fmt
    
    * Parameterize letsencrypt env
    
    * TODO completed
    
    * clippy
    
    * clippy
    
    * A serial for the letsencrypt cert
    
    * Work with local-dev
    
    * Clippy says
    
    * a TODO
    
    * Added a cache so that we don't try to issue a new cert everytime
    
    * warnings
    
    * Create the directory
    
    * sigh, ~/ isn't expanded by anyone
    
    * Include the LE env in the cache key
    
    * clippy says
    
    * Style nit and preemptively blacklist
    
    * inverted...
    
    * Build a chain...
    
    * Better text
    
    * Use upstream API

---
+++ src/letsencrypt.rs
@@ -0,0 +1,265 @@
+use super::common::sha256_hex;
+
+use acme_client;
+use chrono;
+use openssl;
+use rustls;
+use std;
+use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io::{Cursor, Read, Write};
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+
+
+pub trait CertificateCache: Send + Sync {
+    fn store_certificate(&self, identifier: &str, chain: &str, private_key: &str);
+    fn fetch_certificate(&self, identifier: &str) -> Option<(String, String)>;
+}
+
+fn domains_to_identifier(acme_url: &str, domains: &[String]) -> String {
+    sha256_hex(format!("{}|{}", acme_url, domains.join("|")).as_bytes())
+}
+
+pub struct DiskCache {
+    location: PathBuf,
+}
+
+impl DiskCache {
+    pub fn new(location: PathBuf) -> DiskCache {
+        fs::create_dir_all(&location).unwrap();
+        DiskCache { location: location }
+    }
+
+    fn chain_path(&self, identifier: &str) -> PathBuf {
+        self.location.join(identifier.to_string() + ".chain.pem")
+    }
+
+    fn key_path(&self, identifier: &str) -> PathBuf {
+        self.location.join(identifier.to_string() + ".key.pem")
+    }
+}
+
+impl CertificateCache for DiskCache {
+    fn store_certificate(&self, identifier: &str, chain: &str, private_key: &str) {
+        File::create(self.chain_path(identifier))
+            .unwrap()
+            .write_all(chain.as_bytes())
+            .unwrap();
+        File::create(self.key_path(identifier))
+            .unwrap()
+            .write_all(private_key.as_bytes())
+            .unwrap();
+    }
+
+    fn fetch_certificate(&self, identifier: &str) -> Option<(String, String)> {
+        match (File::open(self.chain_path(identifier)), File::open(self.key_path(identifier))) {
+            (Ok(mut chain_file), Ok(mut key_file)) => {
+                let mut chain_pem = String::new();
+                chain_file.read_to_string(&mut chain_pem).unwrap();
+                let mut key_pem = String::new();
+                key_file.read_to_string(&mut key_pem).unwrap();
+                Some((chain_pem, key_pem))
+            }
+            _ => None,
+        }
+    }
+}
+
+pub struct AutomaticCertResolver<C>
+    where C: CertificateCache
+{
+    domains: Vec<String>,
+    acme_url: String,
+    acme_account: acme_client::Account,
+    active_cert: Mutex<Option<rustls::sign::CertChainAndSigner>>,
+    cert_cache: C,
+    sni_challenges: Mutex<HashMap<String, rustls::sign::CertChainAndSigner>>,
+}
+
+
+impl<C> AutomaticCertResolver<C>
+    where C: CertificateCache
+{
+    pub fn new(acme_url: &str, domains: Vec<String>, cache: C) -> AutomaticCertResolver<C> {
+        let acme_directory = acme_client::Directory::from_url(acme_url).unwrap();
+        let pems = cache.fetch_certificate(&domains_to_identifier(acme_url, &domains));
+        let active_cert = Mutex::new(pems.map(|(chain_pem, private_key_pem)| {
+                                                  pems_to_rustls(&chain_pem, &private_key_pem)
+                                              }));
+        AutomaticCertResolver {
+            domains: domains,
+            cert_cache: cache,
+            acme_url: acme_url.to_string(),
+            acme_account: acme_directory.account_registration().register().unwrap(),
+            active_cert: active_cert,
+            sni_challenges: Mutex::new(HashMap::new()),
+        }
+    }
+
+    fn obtain_new_certificate(&self) {
+        // Can't do the smart thing of setting them all up, and then triggering the validations in
+        // parallel and waiting for the results because acme-client doesn't expose seperate
+        // "trigger validation" and "wait for success" functions.
+        for domain in &self.domains {
+            let authorization = self.acme_account.authorization(domain).unwrap();
+            let tls_sni_challenge = authorization.get_tls_sni_challenge().unwrap();
+            self.setup_sni_challenge(tls_sni_challenge);
+            tls_sni_challenge.validate().unwrap();
+            self.teardown_sni_challenge(tls_sni_challenge);
+        }
+        let cert = self.acme_account
+            .certificate_signer(&self.domains
+                                     .iter()
+                                     .map(|s| s.as_ref())
+                                     .collect::<Vec<&str>>())
+            .sign_certificate()
+            .unwrap();
+        // TODO: intermediates
+        let chain = vec![openssl_cert_to_rustls(cert.cert())];
+        let signer = openssl_pkey_to_rustls_signer(cert.pkey());
+        *self.active_cert.lock().unwrap() = Some((chain, Arc::new(signer)));
+        self.cert_cache
+            .store_certificate(&domains_to_identifier(&self.acme_url, &self.domains),
+                               std::str::from_utf8(&cert.cert().to_pem().unwrap()).unwrap(),
+                               // TODO: ECDSA
+                               std::str::from_utf8(&cert.pkey()
+                                                        .rsa()
+                                                        .unwrap()
+                                                        .private_key_to_pem()
+                                                        .unwrap())
+                                       .unwrap());
+    }
+
+    fn setup_sni_challenge(&self, challenge: &acme_client::Challenge) {
+        let z_domain = z_domain(challenge);
+        let (cert, pkey) = generate_temporary_cert(&z_domain);
+
+        let chain = vec![openssl_cert_to_rustls(&cert)];
+        let signer = openssl_pkey_to_rustls_signer(&pkey);
+        self.sni_challenges
+            .lock()
+            .unwrap()
+            .insert(z_domain, (chain, Arc::new(signer)));
+    }
+
+    fn teardown_sni_challenge(&self, challenge: &acme_client::Challenge) {
+        let z_domain = z_domain(challenge);
+        self.sni_challenges.lock().unwrap().remove(&z_domain);
+    }
+}
+
+fn z_domain(challenge: &acme_client::Challenge) -> String {
+    let z = sha256_hex(challenge.key_authorization().as_bytes());
+    let (z1, z2) = z.split_at(32);
+    return format!("{}.{}.acme.invalid", z1, z2);
+}
+
+pub fn generate_temporary_cert(domain: &str) -> (openssl::x509::X509, openssl::pkey::PKey) {
+    let pkey = openssl::pkey::PKey::from_rsa(openssl::rsa::Rsa::generate(2048).unwrap()).unwrap();
+    let mut cert_builder = openssl::x509::X509Builder::new().unwrap();
+    cert_builder.set_version(2).unwrap();
+    cert_builder.set_pubkey(&pkey).unwrap();
+
+    let mut serial = openssl::bn::BigNum::new().unwrap();
+    serial
+        .rand(128, openssl::bn::MSB_MAYBE_ZERO, false)
+        .unwrap();
+    cert_builder
+        .set_serial_number(&serial.to_asn1_integer().unwrap())
+        .unwrap();
+
+    let mut subject_builder = openssl::x509::X509NameBuilder::new().unwrap();
+    subject_builder
+        .append_entry_by_text("CN", "ACME SNI Challenge Certificate")
+        .unwrap();
+    let subject = subject_builder.build();
+    cert_builder.set_subject_name(&subject).unwrap();
+    cert_builder.set_issuer_name(&subject).unwrap();
+
+    cert_builder
+        .set_not_before(&openssl::asn1::Asn1Time::days_from_now(0).unwrap())
+        .unwrap();
+    cert_builder
+        .set_not_after(&openssl::asn1::Asn1Time::days_from_now(1).unwrap())
+        .unwrap();
+
+    let mut san = openssl::x509::extension::SubjectAlternativeName::new();
+    san.dns(domain);
+    let san_ext = san.build(&cert_builder.x509v3_context(None, None)).unwrap();
+    cert_builder.append_extension(san_ext).unwrap();
+
+    cert_builder
+        .sign(&pkey, openssl::hash::MessageDigest::sha256())
+        .unwrap();
+
+    (cert_builder.build(), pkey)
+}
+
+pub fn openssl_cert_to_rustls(cert: &openssl::x509::X509) -> rustls::Certificate {
+    rustls::Certificate(cert.to_der().unwrap())
+}
+
+pub fn openssl_pkey_to_rustls(pkey: &openssl::pkey::PKey) -> rustls::PrivateKey {
+    rustls::PrivateKey(pkey.rsa().unwrap().private_key_to_der().unwrap())
+}
+
+fn openssl_pkey_to_rustls_signer(pkey: &openssl::pkey::PKey) -> Box<rustls::sign::Signer> {
+    // TODO: ECDSA
+    Box::new(rustls::sign::RSASigner::new(&openssl_pkey_to_rustls(pkey)).unwrap())
+}
+
+fn pems_to_rustls(chain_pem: &str, private_key_pem: &str) -> rustls::sign::CertChainAndSigner {
+    let chain = rustls::internal::pemfile::certs(&mut Cursor::new(chain_pem)).unwrap();
+    // TODO: ECDSA
+    let private_key =
+        &rustls::internal::pemfile::rsa_private_keys(&mut Cursor::new(private_key_pem)).unwrap()[0];
+    (chain, Arc::new(Box::new(rustls::sign::RSASigner::new(private_key).unwrap())))
+}
+
+impl<C> rustls::ResolvesServerCert for AutomaticCertResolver<C>
+    where C: CertificateCache
+{
+    fn resolve(&self,
+               server_name: Option<&str>,
+               _: &[rustls::SignatureScheme])
+               -> Option<rustls::sign::CertChainAndSigner> {
+
+        if let Some(sni) = server_name {
+            if let Some(cert) = self.sni_challenges.lock().unwrap().get(sni) {
+                return Some(cert.clone());
+            }
+        }
+        // Seperate scope so that the lock isn't held we enter `obtain_new_certificate`.
+        {
+            let active_cert = self.active_cert.lock().unwrap();
+            if cert_is_valid(&active_cert) {
+                // TODO: if it's _close_ to expiring, trigger a background "obtain new cert"
+                return active_cert.clone();
+            }
+        }
+        // TODO: Don't try to obtain a new cert if we're currently waiting for one already...
+        self.obtain_new_certificate();
+        self.active_cert.lock().unwrap().clone()
+    }
+}
+
+fn cert_is_valid(cert: &Option<rustls::sign::CertChainAndSigner>) -> bool {
+    match *cert {
+        Some((ref chain, _)) => !cert_is_expired(&chain[0]),
+        None => false,
+    }
+}
+
+fn cert_is_expired(cert: &rustls::Certificate) -> bool {
+    let openssl_cert = openssl::x509::X509::from_der(&cert.0).unwrap();
+    from_asn1_time(openssl_cert.not_after()) < chrono::UTC::now()
+}
+
+
+fn from_asn1_time(t: &openssl::asn1::Asn1TimeRef) -> chrono::DateTime<chrono::UTC> {
+    let dt = chrono::DateTime::parse_from_str(&t.to_string().replace(" GMT", " +00:00"),
+                                              "%b %e %T %Y %z")
+            .unwrap();
+    chrono::DateTime::<chrono::UTC>::from_utc(dt.naive_utc(), chrono::UTC)
+}
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,25 @@ extern crate hyper_rustls;
 extern crate pem;
 #[macro_use]
 extern crate prettytable;
+extern crate ring;
+extern crate rustls;
 extern crate serde_json;
+extern crate tempfile;
+#[macro_use]
+extern crate tera;
 
 extern crate ct_tools;
 
-
-use ct_tools::{censys, crtsh};
+use ct_tools::{censys, crtsh, letsencrypt};
+use ct_tools::common::{Log, sha256_hex};
 use ct_tools::ct::submit_cert_to_logs;
 use ct_tools::google::fetch_trusted_ct_logs;
+use std::env;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
+use std::process::Command;
+use std::sync::Arc;
+use std::time::Duration;
 
 
 fn pems_to_chain(data: &str) -> Vec<Vec<u8>> {
@@ -86,6 +95,145 @@ fn check(paths: clap::Values) {
     }
 }
 
+struct HttpHandler {
+    templates: tera::Tera,
+    http_client: hyper::Client,
+    logs: Vec<Log>,
+}
+
+impl hyper::server::Handler for HttpHandler {
+    fn handle(&self, request: hyper::server::Request, response: hyper::server::Response) {
+        let peer_certs = request
+            .ssl::<hyper_rustls::WrappedStream>()
+            .unwrap()
+            .to_tls_stream()
+            .get_session()
+            .get_peer_certificates();
+
+        let mut crtsh_url = None;
+        if peer_certs.is_some() && request.method == hyper::method::Method::Post {
+            let chain = crtsh::build_chain_for_cert(&self.http_client,
+                                                    &peer_certs.as_ref().unwrap()[0].0);
+            let scts = submit_cert_to_logs(&self.http_client, &self.logs, &chain);
+            if !scts.is_empty() {
+                crtsh_url = Some(crtsh::url_for_cert(&chain[0]));
+                println!("Successfully submitted: {}", sha256_hex(&chain[0]));
+            }
+        }
+
+        let rendered_cert = peer_certs.map(|chain| {
+            let mut tmp = tempfile::NamedTempFile::new().unwrap();
+            tmp.write_all(&chain[0].0).unwrap();
+            tmp.flush().unwrap();
+            let stdout = Command::new("openssl")
+                .arg("x509")
+                .arg("-text")
+                .arg("-noout")
+                .arg("-inform")
+                .arg("der")
+                .arg("-in")
+                .arg(tmp.path().as_os_str())
+                .output()
+                .unwrap()
+                .stdout;
+            String::from_utf8_lossy(&stdout).into_owned()
+        });
+
+        let mut context = tera::Context::new();
+        context.add("cert", &rendered_cert);
+        context.add("crtsh_url", &crtsh_url);
+        response
+            .send(self.templates
+                      .render("home.html", &context)
+                      .unwrap()
+                      .as_bytes())
+            .unwrap();
+    }
+}
+
+struct NoVerificationCertificateVerifier;
+impl rustls::ClientCertVerifier for NoVerificationCertificateVerifier {
+    fn verify_client_cert(&self,
+                          _: &rustls::RootCertStore,
+                          _: &[rustls::Certificate])
+                          -> Result<(), rustls::TLSError> {
+        Ok(())
+    }
+}
+static NO_VERIFICATION: NoVerificationCertificateVerifier = NoVerificationCertificateVerifier;
+
+
+fn server(local_dev: bool, domain: Option<&str>, letsencrypt_env: Option<&str>) {
+    match (local_dev, domain, letsencrypt_env) {
+        (true, Some(_), _) |
+        (true, _, Some(_)) => {
+            panic!("Can't use both --local-dev and --letsencrypt-env or --domain")
+        }
+        (_, Some(_), None) |
+        (_, None, Some(_)) => {
+            panic!("When using Let's Encrypt, must set both --letsencrypt-env and --domain")
+        }
+        (false, _, None) => panic!("Must set at least one of --local-dev or --letsencrypt-env"),
+        _ => {}
+    };
+
+    let mut tls_config = rustls::ServerConfig::new();
+    tls_config.client_auth_offer = true;
+    if local_dev {
+        // TODO: not all the details on the cert are perfect, but it's fine.
+        let (cert, pkey) = letsencrypt::generate_temporary_cert("localhost");
+        tls_config.set_single_cert(vec![letsencrypt::openssl_cert_to_rustls(&cert)],
+                                   letsencrypt::openssl_pkey_to_rustls(&pkey));
+    } else {
+        let letsencrypt_url = match letsencrypt_env.unwrap() {
+            "prod" => "https://acme-v01.api.letsencrypt.org/directory",
+            "dev" => "https://acme-staging.api.letsencrypt.org/directory",
+            _ => unreachable!(),
+        };
+        let cert_cache = letsencrypt::DiskCache::new(env::home_dir()
+                                                         .unwrap()
+                                                         .join(".ct-tools")
+                                                         .join("certificates"));
+        tls_config.cert_resolver =
+            Box::new(letsencrypt::AutomaticCertResolver::new(letsencrypt_url,
+                                                             vec![domain.unwrap().to_string()],
+                                                             cert_cache));
+    }
+    tls_config.set_persistence(rustls::ServerSessionMemoryCache::new(1024));
+    tls_config.ticketer = rustls::Ticketer::new();
+    // Disable certificate verification. In any normal context, this would be horribly insecure!
+    // However, all we're doing is taking the certs and then turning around and submitting them to
+    // CT logs, so it doesn't matter if they're verified.
+    tls_config
+        .dangerous()
+        .set_certificate_verifier(&NO_VERIFICATION);
+    let tls_server = hyper_rustls::TlsServer { cfg: Arc::new(tls_config) };
+
+    let mut http_client = hyper::Client::with_connector(
+        hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())
+    );
+    let logs = fetch_trusted_ct_logs(&http_client);
+    http_client.set_write_timeout(Some(Duration::from_secs(5)));
+    http_client.set_read_timeout(Some(Duration::from_secs(5)));
+    let handler = HttpHandler {
+        templates: compile_templates!("templates/*"),
+        http_client: http_client,
+        logs: logs,
+    };
+
+    let addr = if local_dev {
+        "127.0.0.1:1337"
+    } else {
+        "0.0.0.0:443"
+    };
+    println!("Listening on https://{} ...", addr);
+    hyper::Server::https(addr, tls_server)
+        .unwrap()
+        // If there aren't at least two threads, the Let's Encrypt integration will deadlock.
+        .handle_threads(handler, 16)
+        .unwrap();
+}
+
 fn main() {
     let matches = clap::App::new("ct-tools")
         .subcommand(clap::SubCommand::with_name("submit")
@@ -100,11 +248,29 @@ fn main() {
                                  .multiple(true)
                                  .required(true)
                                  .help("Path to certificate or chain")))
+        .subcommand(clap::SubCommand::with_name("server")
+                        .about("Run an HTTPS server that submits client to CT logs")
+                        .arg(clap::Arg::with_name("local-dev")
+                                 .long("--local-dev")
+                                 .help("Local development, do not obtain a certificate"))
+                        .arg(clap::Arg::with_name("domain")
+                                 .takes_value(true)
+                                 .long("--domain")
+                                 .help("Domain this is running as"))
+                        .arg(clap::Arg::with_name("letsencrypt-env")
+                                 .takes_value(true)
+                                 .long("--letsencrypt-env")
+                                 .possible_values(&["dev", "prod"])
+                                 .help("Let's Encrypt environment to use")))
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("submit") {
         submit(matches.values_of("path").unwrap());
     } else if let Some(matches) = matches.subcommand_matches("check") {
         check(matches.values_of("path").unwrap());
+    } else if let Some(matches) = matches.subcommand_matches("server") {
+        server(matches.is_present("local-dev"),
+               matches.value_of("domain"),
+               matches.value_of("letsencrypt-env"));
     }
 }
