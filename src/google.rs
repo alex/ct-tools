e4311b79140206929166dab4f41281cddac4fa2a
Alex Gaynor <alex.gaynor@gmail.com>
2017-06-20 21:19:18 -0400

    cargo fmt

--- src/google.rs
+++ src/google.rs
@@ -43,11 +43,11 @@ pub fn fetch_trusted_ct_logs(http_client: &hyper::Client) -> Vec<Log> {
         .into_iter()
         .filter(|log| log.disqualified_at.is_none())
         .map(|log| {
-                 Log {
-                     url: log.url,
-                     description: log.description,
-                     is_google: log.operated_by.contains(&google_id.unwrap()),
-                 }
-             })
+            Log {
+                url: log.url,
+                description: log.description,
+                is_google: log.operated_by.contains(&google_id.unwrap()),
+            }
+        })
         .collect()
 }
