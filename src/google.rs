73982a91ffd72ac8a6bc9cf4ce90efca9bd80fdb
Alex Gaynor <alex.gaynor@gmail.com>
2017-09-01 18:44:05 -0400

    remove more box

--- src/google.rs
+++ src/google.rs
@@ -27,34 +27,35 @@ struct LogsResponse {
     operators: Vec<LogsResponseOperators>,
 }
 
-#[async]
-pub fn fetch_trusted_ct_logs<C: hyper::client::Connect>(
-    http_client: Box<hyper::Client<C>>,
-) -> Result<Vec<Log>, ()> {
-    let response = await!(http_client.get(LOG_LIST_URL.parse().unwrap())).unwrap();
-    // Limit the response to 10MB at most, to be resillient to DoS.
-    let body = await!(response.body().take(10 * 1024 * 1024).concat2()).unwrap();
-    let logs_response: LogsResponse = serde_json::from_slice(&body).unwrap();
+pub fn fetch_trusted_ct_logs<'a, C: hyper::client::Connect>(
+    http_client: &'a hyper::Client<C>,
+) -> impl Future<Item=Vec<Log>, Error=()> + 'a {
+    async_block! {
+        let response = await!(http_client.get(LOG_LIST_URL.parse().unwrap())).unwrap();
+        // Limit the response to 10MB at most, to be resillient to DoS.
+        let body = await!(response.body().take(10 * 1024 * 1024).concat2()).unwrap();
+        let logs_response: LogsResponse = serde_json::from_slice(&body).unwrap();
 
-    let google_id = logs_response
-        .operators
-        .iter()
-        .find(|o| o.name == "Google")
-        .map(|o| o.id)
-        .unwrap();
+        let google_id = logs_response
+            .operators
+            .iter()
+            .find(|o| o.name == "Google")
+            .map(|o| o.id)
+            .unwrap();
 
-    Ok(
-        logs_response
-            .logs
-            .into_iter()
-            .filter(|log| log.disqualified_at.is_none())
-            .map(move |log| {
-                Log {
-                    url: log.url,
-                    description: log.description,
-                    is_google: log.operated_by.contains(&google_id),
-                }
-            })
-            .collect(),
-    )
+        Ok(
+            logs_response
+                .logs
+                .into_iter()
+                .filter(|log| log.disqualified_at.is_none())
+                .map(move |log| {
+                    Log {
+                        url: log.url,
+                        description: log.description,
+                        is_google: log.operated_by.contains(&google_id),
+                    }
+                })
+                .collect(),
+        )
+    }
 }
