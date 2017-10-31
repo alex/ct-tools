61536b5d11a03fdf54268ba669fa1333b5d44052
Alex Gaynor <alex.gaynor@gmail.com>
2017-10-31 18:52:13 -0400

    clippy lint

--- src/google.rs
+++ src/google.rs
@@ -6,8 +6,8 @@ use hyper;
 use serde_json;
 
 
-const TRUSTED_LOG_LIST_URL: &'static str = "https://www.gstatic.com/ct/log_list/log_list.json";
-const ALL_LOG_LIST_URL: &'static str = "https://www.gstatic.com/ct/log_list/all_logs_list.json";
+const TRUSTED_LOG_LIST_URL: &str = "https://www.gstatic.com/ct/log_list/log_list.json";
+const ALL_LOG_LIST_URL: &str = "https://www.gstatic.com/ct/log_list/all_logs_list.json";
 
 #[derive(Deserialize)]
 struct LogsResponseLogs {
